7a488f21e1a4c8f3f92d33ae1057b62a672596a5
yanpak <yanpak@yandex.ru>
2014-10-17 02:01:50 +0400

    Yet one interesting variant from me

--- src/StringExchanger.java
+++ src/StringExchanger.java
@@ -5,8 +5,12 @@ public class StringExchanger {
 
     public static void main(String[] args) {
         //a=a.concat(b).split("\\b"+(a.substring(0,(b=a).length())))[1]; //Alright!
-        a=a.concat(b).substring((b=a).length()); // My new variant is more neat )
-        System.out.printf("%s %s",a,b);
+
+        //a=a.concat(b).substring((b=a).length()); // My new variant is more neat )
+
+        a = new String[]{b, b=a}[0]; // yet one interesting variant from me!
+
+        System.out.printf("%s %s", a, b);
     }
 
 }
