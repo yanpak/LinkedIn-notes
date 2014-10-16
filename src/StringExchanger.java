f58f3bc93516db8eddba73eef4dbb70d42e1f335
yanpak <yanpak@yandex.ru>
2014-10-17 01:43:50 +0400

    New variant of solution

--- src/StringExchanger.java
+++ src/StringExchanger.java
@@ -1,10 +1,11 @@
 public class StringExchanger {
 
     static String a = "aaa";
-    static String b = "bbbbaaa";
+    static String b = "bbbb";
 
     public static void main(String[] args) {
-        a=a.concat(b).split("\\b"+(a.substring(0,(b=a).length())))[1]; //Alright!
+        //a=a.concat(b).split("\\b"+(a.substring(0,(b=a).length())))[1]; //Alright!
+        a=a.concat(b).substring((b=a).length()); // My new variant is more neat )
         System.out.printf("%s %s",a,b);
     }
 
