8a451b8955438b99509442681fa9cb94028eb023
yanpak <yanpak@yandex.ru>
2014-10-17 01:30:14 +0400

    added word boundary to regexp

--- src/StringExchanger.java
+++ src/StringExchanger.java
@@ -1,10 +1,10 @@
 public class StringExchanger {
 
     static String a = "aaa";
-    static String b = "bbbb";
+    static String b = "bbbbaaa";
 
     public static void main(String[] args) {
-        a=a.concat(b).split(a.substring(0,(b=a).length()))[1]; //Alright!
+        a=a.concat(b).split("\\b"+(a.substring(0,(b=a).length())))[1]; //Alright!
         System.out.printf("%s %s",a,b);
     }
 
